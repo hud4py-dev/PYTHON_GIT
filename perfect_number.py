9539789147f1765754d19e9fb34480bf33ca1d58
hud@ <hud4.py@gmail.com>
2025-08-20 00:21:45 +0700

    Update perfect_number.py

--- perfect_number.py
+++ perfect_number.py
@@ -1,1 +1,23 @@
+# perfect number checking
 
+import math
+
+def dvsr(intgr):
+  div = []
+  count = 1
+  while (count<intgr):
+    if (intgr%count) == 0:
+      div.append(intgr)
+    count += 1
+  cek(div)
+
+def check(list):
+  s = sum(list)
+  if intgr == s:
+    print(x,"is a perfect number")
+  else:
+    print(x,"is not a perfect number")
+
+if __name__ == "__main__":
+x = int(input("number : "))
+dvsr(x)
